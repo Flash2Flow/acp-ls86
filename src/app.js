90366fd523032af444c1ed9d694ae9abff3d0501
shot <shakido@mail.ru>
2022-07-10 13:14:39 +0300

    add

--- src/app.js
+++ src/app.js
@@ -11,9 +11,73 @@ $('#btn-close-player').on('click',function(){
 $('#open-read').on('click',function(){
     $('#block-module').show(300);
 })
+$('#btn-find').on('click',function(){
+    $('#block-find').show(300);
+    return false;
+})
+$('#open-settings-player').on('click',function(){
+    $('#block-tipes').hide(0);
+    $('#block-settings-player').show(300);
+    return false;
+})
+$('#open-block-ban').on('click',function(){
+    $('#block-retest').hide(0);
+    $('#block-warn').hide(0);
+    $('#block-ban').show(300);
+    return false;
+})
+$('#open-block-warn').on('click',function(){
+    $('#block-warn').show(300);
+    $('#block-ban').hide(0);
+    $('#block-retest').hide(0);
+    return false;
+})
+$('#open-block-retest').on('click',function(){
+    $('#block-retest').show(300);
+    $('#block-warn').hide(0);
+    $('#block-ban').hide(0);
+    return false;
+})
+$('#player-one').on('click',function(){
+    $('#block-player-set').show(300);
+    $('#block-settings-player').hide(0);
+    return false;
+})
+$('#close-block').on('click',function(){
+    $('#block-tipes').show(300);
+    $('#block-player-set').hide(0);
+    return false;
+})
+
+
 
 $(document).click( function(event){
     if( $(event.target).closest("#block-hot").length ) return;
     $("#block-hot").hide(300);
+    event.stopPropagation();
+  });
+  $(document).click( function(event){
+    if( $(event.target).closest("#block-find").length ) return;
+    $("#block-find").hide(300);
+    event.stopPropagation();
+  });
+  $(document).click( function(event){
+    if( $(event.target).closest("#block-settings-player").length ) return;
+    $("#block-settings-player").hide(300);
+    event.stopPropagation();
+  });
+  $(document).click( function(event){
+    if( $(event.target).closest("#block-ban").length ) return;
+    $("#block-ban").hide(300);
+    event.stopPropagation();
+  });
+  $(document).click( function(event){
+    if( $(event.target).closest("#block-warn").length ) return;
+    $("#block-warn").hide(300);
+    event.stopPropagation();
+  });
+  $(document).click( function(event){
+    if( $(event.target).closest("#block-retest").length ) return;
+    $("#block-retest").hide(300);
     event.stopPropagation();
   });
